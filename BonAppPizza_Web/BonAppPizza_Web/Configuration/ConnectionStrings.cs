815c19dc7119df5b0725f4bbc636b34bc72f68f8
Gabriel Vieira <luizcarlosdesouza@rocketmail.com>
2018-11-26 20:55:32 -0200

    Segunda-feira, 26-11-2018

---
+++ BonAppPizza_Web/BonAppPizza_Web/Configuration/ConnectionStrings.cs
@@ -0,0 +1,9 @@
+﻿namespace BonAppPizza_Web.Configuration
+{
+    public class ConnectionStrings
+    {
+        public string ApiDb { get; set; }
+
+        public string Api2Db { get; set; }
+    }
+}
---
+++ BonAppPizza_Web/BonAppPizza_Web/SwaggerExamples/FooListResponseExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using BonAppPizza_Web.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace BonAppPizza_Web.SwaggerExamples
+{
+    public class FooListResponseExample : IExamplesProvider
+    {
+        public object GetExamples()
+        {
+            return new List<Foo>
+            {
+                new Foo { Id = new Random().Next(), Value = Guid.NewGuid().ToString().Remove(6)},
+                new Foo { Id = new Random().Next(), Value = Guid.NewGuid().ToString().Remove(6)}
+            };
+        }
+    }
+}
