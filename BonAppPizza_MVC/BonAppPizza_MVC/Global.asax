9898aea6a6b0a148bec5f64688794591019168ae
lcsouza0710 <luizcarlosdesouza@rocketmail.com>
2018-11-28 19:16:29 -0200

    Quarta-feira, 28-11-2018 (Criação do projeto MVC)

---
+++ BonAppPizza_MVC/BonAppPizza_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BonAppPizza_MVC.Global" Language="C#" %>
---
+++ BonAppPizza_Web/BonAppPizza_Web/Controllers/TesteLayoutController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BonAppPizza_Web.Controllers
+{
+    public class TesteLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
