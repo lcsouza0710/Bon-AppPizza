63473cf933667d967e892088cecb43d602663258
lcsouza0710 <luizcarlosdesouza@rocketmail.com>
2018-12-02 05:10:49 -0200

    Deletar pastas MVC e WEB

--- BonAppPizza_Web/BonAppPizza_Web/Configuration/ConnectionStrings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BonAppPizza_Web.Configuration
-{
-    public class ConnectionStrings
-    {
-        public string ApiDb { get; set; }
-
-        public string Api2Db { get; set; }
-    }
-}
